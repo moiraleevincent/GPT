78cbf0fa509ce9ab2470ce8011ba4a5706108567
moiraleevincent <moiraleevincent@gmail.com>
2026-09-24 18:00:33 +0200

    Add Voice 2 documentary audition

--- voice_demo/audition.py
+++ voice_demo/audition.py
@@ -19,5 +19,6 @@ def make(path, second):
     sf.write(path,np.concatenate(out),24000)
 make('voice_demo/audition_A.wav','af_heart')
 make('voice_demo/audition_B.wav','am_adam')
+make('voice_demo/audition_voice2.wav','af_bella')
 
 # generate blind A/B pair
